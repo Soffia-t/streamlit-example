3f5ec7a3f2cc9a59a68cc1545b340c5f4be85bf9
Soffia-t <103429642+Soffia-t@users.noreply.github.com>
2023-07-31 08:12:22 +0300

    Created using Colaboratory

---
+++ app.py
@@ -0,0 +1,130 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOus0DsVIvtk50CAResyW/+",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Soffia-t/streamlit-example/blob/master/app.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "MkS53pz7RFJB"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# Creating an interactive app with a rotating globe using Streamlit, Plotly, Colab, and activeloop requires some integration between these libraries. While Streamlit and Plotly can work together, Colab and activeloop are typically used in Jupyter notebooks or for specific machine learning tasks.\n",
+        "\n",
+        "# As of my last update in September 2021, Streamlit and Plotly do not support interactive 3D globe visualization natively. However, you can achieve this by embedding a Plotly 3D globe figure into your Streamlit app.\n",
+        "\n",
+        "# Here's a high-level overview of how you can go about building the app:\n",
+        "\n",
+        "# 1. **Install Required Libraries:**\n",
+        "# Make sure you have Streamlit and Plotly installed. If you're using Google Colab, you might not need to install them separately as they are usually pre-installed.\n",
+        "\n",
+        "# 2. **Create the Interactive Globe with Plotly:**\n",
+        "# Use Plotly to create an interactive 3D globe visualization. The `plotly.graph_objects` module provides functionality to create 3D scatter plots, which can be used to visualize data on a globe. The plot can be rotated interactively by users.\n",
+        "\n",
+        "# 3. **Integrate with Streamlit:**\n",
+        "# Streamlit allows you to embed Plotly figures using the `st.plotly_chart()` function. You can pass the Plotly figure to this function to display the 3D globe in your Streamlit app.\n",
+        "\n",
+        "# 4. **Deploy the Streamlit App:**\n",
+        "# Once you have developed the Streamlit app locally, you can deploy it to various platforms like Streamlit Sharing or Heroku so that others can access it over the internet.\n",
+        "\n",
+        "# Here's a basic code example of how you can integrate Plotly and Streamlit to create the interactive globe:\n",
+        "\n",
+        "# ```python\n",
+        "# app.py\n",
+        "import streamlit as st\n",
+        "import plotly.graph_objects as go\n",
+        "\n",
+        "def create_globe():\n",
+        "    # Replace the following sample data with your own data if you want to visualize specific points on the globe.\n",
+        "    # latitude, longitude, and text are lists containing respective values for each point.\n",
+        "    latitudes = [0, 30, 60]\n",
+        "    longitudes = [0, 45, -30]\n",
+        "    texts = [\"Point 1\", \"Point 2\", \"Point 3\"]\n",
+        "\n",
+        "    fig = go.Figure(data=go.Scattergeo(\n",
+        "        lat=latitudes,\n",
+        "        lon=longitudes,\n",
+        "        text=texts,\n",
+        "        mode='markers',\n",
+        "        marker=dict(\n",
+        "            size=10,\n",
+        "            color='red',\n",
+        "            opacity=0.8,\n",
+        "            line=dict(\n",
+        "                width=2,\n",
+        "                color='black'\n",
+        "            )\n",
+        "        )\n",
+        "    ))\n",
+        "\n",
+        "    # Update the layout to create an interactive 3D globe view.\n",
+        "    fig.update_geos(projection_type=\"orthographic\")\n",
+        "    fig.update_layout(\n",
+        "        title=\"Interactive Globe\",\n",
+        "        geo=dict(\n",
+        "            showland=True,\n",
+        "            showocean=True,\n",
+        "            oceancolor='lightblue',\n",
+        "            showcountries=True\n",
+        "        )\n",
+        "    )\n",
+        "\n",
+        "    return fig\n",
+        "\n",
+        "def main():\n",
+        "    st.title(\"Interactive Globe App\")\n",
+        "    globe_fig = create_globe()\n",
+        "    st.plotly_chart(globe_fig)\n",
+        "\n",
+        "if __name__ == \"__main__\":\n",
+        "     main()\n",
+        "\n",
+        "# ```\n",
+        "\n",
+        "# To run the app, save the above code in a file named `app.py`, and then run it using Streamlit:\n",
+        "\n",
+        "# ```\n",
+        "# streamlit run app.py\n",
+        "# ```\n",
+        "\n",
+        "# Remember that this example uses sample data for illustration purposes. You can replace the data with your own dataset to visualize specific points on the globe.\n",
+        "\n",
+        "# Unfortunately, Google Colab and activeloop are not directly related to this specific task of creating an interactive globe visualization, so their integration might not be straightforward. However, if you have any specific tasks in mind for Colab and activeloop, feel free to ask, and I'll be happy to assist!"
+      ],
+      "metadata": {
+        "id": "au9ntZ9NWyaj"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
